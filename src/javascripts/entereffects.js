cb4207c37501eacd0bb5a28fdf11340d501d174f
elujambio <eugeniolujambio@gmail.com>
2016-04-27 15:20:22 -0500

    Website oriented left

---
+++ src/javascripts/entereffects.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal();
+sr.reveal();
+sr.reveal('.info-3', { duration: 2000, origin: "right", scale: 0, distance:"100px" }, 500);
+sr.reveal('.intro-box', { duration: 1000, origin: "right", scale: 0, distance: "50px" }, 1000);
+sr.reveal('.header-2', { duration:1000, origin: "right", scale: 0, distance: "50px" }, 300); 
+sr.reveal('.header-3', { duration:1000, origin: "right", scale: 0, distance: "50px" }, 300); 
+sr.reveal('.header-4', { duration:1000, origin: "right", scale: 0, distance: "50px" }, 300); 
+sr.reveal('.form-intro', { duration:600, origin: "right", scale: 0, distance: "50px" }, 300); 
